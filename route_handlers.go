1855bcc39bd9c0942d2cd4547f576d64b173a489
James Andrews <bdaddyja@gmail.com>
2018-06-02 01:40:06 -0700

    SignUp route handler now populates Credentials struct.

--- route_handlers.go
+++ route_handlers.go
@@ -1,37 +1,51 @@
 package main
 
 import (
+  "encoding/json"
+  "io/ioutil"
   "net/http"
   "fmt"
 )
 
 type Credentials struct {
-  Username string `json: "username"`
+  Username string `json: username`
   Password string `json: "password"`
 }
 
-var users []Credentials
-
 func SignUp(w http.ResponseWriter, r *http.Request) {
-  user := Credentials{}
 
-  err := r.ParseForm()
+  b, err := ioutil.ReadAll(r.Body)
+  defer r.Body.Close()
+
+  var info Credentials
 
   if err != nil {
-    fmt.Println(fmt.Errorf("Error: %v", err))
-    w.WriteHeader(http.StatusInternalServerError)
-
+    http.Error(w, err.Error(), 500)
     return
   }
 
-  user.Username = r.Form.Get("username")
-  user.Password = r.Form.Get("password")
+  user := Credentials{}
+  err = json.Unmarshal(b, &info)
+
+  if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+  Username, err := info.Username, nil
+  Password, err := info.Password, nil
+
+  if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+  user.Username = string(Username)
+  user.Password = string(Password)
 
   err = store.CreateUser(&user)
 
   if(err != nil) {
     fmt.Println(err)
   }
-
-  http.Redirect(w, r, "/signup", http.StatusFound)
 }
