8d46f2eb1bda606496b3d37c3ad644323b7e8afd
James Andrews <bdaddyja@gmail.com>
2018-06-03 01:22:08 -0700

    (WIP)  SignInUser queries database twice to validate user.  Might be a better way to do this.

--- store.go
+++ store.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "net/http"
+  "log"
   "database/sql"
   "fmt"
 _"github.com/lib/pq"
@@ -9,6 +11,7 @@ _"github.com/lib/pq"
 
 type Store interface {
   CreateUser(user *Credentials) error
+  SignInUser(user *Credentials, w http.ResponseWriter) error
 }
 
 type dbStore struct {
@@ -33,6 +36,42 @@ func (store *dbStore) CreateUser(creds *Credentials) error {
 
   if err != nil {
     fmt.Println(err)
+  }
+
+  return err
+}
+
+func (store *dbStore) SignInUser(creds *Credentials, w http.ResponseWriter) error {
+  var username string
+
+  results, err := store.db.Query("Select password FROM users where username=$1", creds.Username)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  err = store.db.QueryRow("Select password FROM users where username=$1", creds.Username).Scan(&username)
+
+  if err != nil {
+    log.Printf("Error Code: %d",http.StatusUnauthorized)
+    return err
+  }
+
+  defer results.Close()
+
+  for results.Next() {
+    var password string
+
+    err := results.Scan(&password)
+
+    if err != nil {
+      w.WriteHeader(http.StatusUnauthorized)
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(password), []byte(creds.Password)); err != nil {
+      log.Printf("Error Code: %d",http.StatusUnauthorized)
+      return err
+    }
   }
   return err
 }
